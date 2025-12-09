e88dd56e95581d4fbee5d54a6c2ba35956ac2a9f
maanika-m <maanikamishr@gmail.com>
2025-12-10 05:25:19 +0530

    did 5 pattern prbs

---
+++ pattern/prb3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class prb3 {
+    public static void main(String[] args) {
+        int row = 5;
+        for (int i = 0; i<row; i++){
+            for (int j=0; j<=i; j++){
+                    System.out.print(j+1);
+                }
+        System.out.println();  
+    }
+ }
+}
---
+++ pattern/prb5.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class prb5 {
+    public static void main(String[] args) {
+        int row = 5;
+        for (int i = 0; i<row; i++){
+            for (int j=1; j<=row-i; j++){
+                System.out.print("*");
+            }
+          System.out.println();  
+        }
+
+    }
+    
+}
